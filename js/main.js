72269e048892aa5e5f31a7e8006b333d7c2a5e9a
Antoniok2 <antoniopiazza18@gmail.com>
2021-11-12 14:56:24 +0100

    add event deleteTask

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Descrizione:
 // Rifare l’esercizio della to do list:
-// - stampare in pagina un item per ogni elemento contenuto in un array
+// - stampare in pagina un item per ogni elemento contenuto in un array V
 // - ogni item ha una “x” associata: cliccando su di essa, l’item viene rimosso dalla lista
 // - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
 var app = new Vue(
@@ -34,6 +34,11 @@ var app = new Vue(
                 }
             ]
         },
+        methods: {
+            removeTask(index){
+                this.tasks.splice(index,1)
+            }
+        }
 
     }
 )
