8dca1c9331864dc4a4921e8a53e331e5a821447d
Antoniok2 <antoniopiazza18@gmail.com>
2021-11-12 14:48:56 +0100

    add tasks with vue

--- js/main.js
+++ js/main.js
@@ -3,3 +3,37 @@
 // - stampare in pagina un item per ogni elemento contenuto in un array
 // - ogni item ha una “x” associata: cliccando su di essa, l’item viene rimosso dalla lista
 // - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+var app = new Vue(
+    {
+        el: "#myapp",
+        data: {
+            tasks: [
+                {
+                    text: 'Avviare lezione in classe',
+                    done: 'true'
+                },
+                {
+                    text: 'Pranzare',
+                    done: 'true'
+                },
+                {
+                    text: 'Fare sercizio assegnato',
+                    done: 'false'
+                },
+                {
+                    text:'Andare a lavorare',
+                    done: 'true'
+                },
+                {
+                    text: 'cenare',
+                    done: 'false'
+                },
+                {
+                    text: 'dormire',
+                    done: 'true'
+                }
+            ]
+        },
+
+    }
+)
